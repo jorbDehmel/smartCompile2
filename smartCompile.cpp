2fa448ae74cc4f2bf0c0fd3918f037eaa6b2ce73
jorbDehmel <jdehmel@outlook.com>
2022-12-13 15:16:28 -0700

    stuff

--- smartCompile.cpp
+++ smartCompile.cpp
@@ -2,29 +2,21 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <boost/regex.hpp>
+#include <assert.h>
 
 using namespace std;
 
-// Recursive ls without folder names (except "./"):
+using boost::regex;
+using boost::regex_match;
+using boost::regex_replace;
+
 // find -print >output.txt
 
-void makeObjs(vector<string> &includes, string &stem) {
-
+void makeObjs()
+{
 }
 
-vector<string> makeBin(vector<string> &objs, string &stem) {
-
-}
-
-int main(char *argv[], int argc) {
-    vector<string> includes;
-
-    // find all .cpp
-    // for all without main:
-        // make objs
-    // for all with main:
-        // make obj
-        // build bin w/ all .o that don't have main
-
-    return 0;
+void link()
+{
 }
