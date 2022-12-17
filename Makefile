b4d2db63410b4ed4557ed0ff15a08c354d4460a9
jorbDehmel <jdehmel@outlook.com>
2022-12-17 15:36:42 -0700

    Expanded compatability

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 sc.out:	smartCompile.cpp
 	clang++ -pedantic -Wall -lboost_regex smartCompile.cpp -o sc.out
 
+install: sc.out
+	sudo cp sc.out /bin/sc
+
 pclean:
 	rm *.out
--- smartCompile.cpp
+++ smartCompile.cpp
@@ -1,3 +1,5 @@
+// SC_ARGS -lboost_regex
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -6,18 +8,12 @@
 #include "tags.hpp"
 
 using namespace std;
-
-using boost::regex;
-using boost::regex_match;
-using boost::regex_replace;
+using namespace boost;
 
 //////////////////////////////////
 
-string BUILD_DIR = "./.build";
-
-string CC = "clang++";
-string ARGS = "-pedantic -Wall";
-string NAME = "./a.out";
+string CC = "gcc";
+string ARGS = "";
 
 //////////////////////////////////
 
@@ -45,14 +41,42 @@ void smartSystem(string command)
 
 //////////////////////////////////
 
+bool isMain(const string &path, string &args)
+{
+    ifstream f(path);
+    if (!f.is_open())
+    {
+        cout << tags::red_bold << "Could not open file.\n"
+             << tags::reset;
+        return false;
+    }
+
+    string text, line;
+
+    getline(f, text);
+    if (regex_match(text, regex("// SC_ARGS .*")))
+    {
+        cout << tags::violet_bold << "Found arguments " << text.substr() << ".\n"
+             << tags::reset;
+
+        args += ' ' + text.substr(11);
+    }
+
+    while (getline(f, line))
+    {
+        text += line + '\n';
+    }
+    f.close();
+
+    return regex_search(text, regex("int main"));
+}
+
 vector<string> makeObjs()
 {
     cout << tags::green_bold
-         << "Preparing to compile...\n\n"
+         << "Preparing to compile...\n"
          << tags::reset;
 
-    string command = "mkdir -p " + BUILD_DIR;
-    smartSystem(command);
     smartSystem("find -print >.build/temp.txt");
 
     ifstream fin(".build/temp.txt");
@@ -66,10 +90,24 @@ vector<string> makeObjs()
         if (regex_match(line, regex(".*?\\.cpp")))
         {
             cpps.push_back(line);
-            objs.push_back(BUILD_DIR + '/' + regex_replace(line, regex("\\.cpp"), ".o"));
 
-            command = CC + ' ' + ARGS + " -c " + line + " -o " + objs[objs.size() - 1];
-            smartSystem(command);
+            if (isMain(line, ARGS))
+            {
+                cout << tags::violet_bold << "File " << line << " has a main.\n"
+                     << tags::reset;
+
+                line = regex_replace(line, regex(".*/"), "");
+                objs.push_back(".build/__MAIN__" + regex_replace(line, regex("\\.cpp"), ".o"));
+            }
+            else
+            {
+                cout << tags::violet_bold << "File " << line << " has no main.\n"
+                     << tags::reset;
+
+                objs.push_back(".build/" + regex_replace(line, regex("\\.cpp"), ".o"));
+            }
+
+            smartSystem(CC + " -c " + line + " -o " + objs[objs.size() - 1]);
         }
     }
 
@@ -77,7 +115,7 @@ vector<string> makeObjs()
     system("rm .build/temp.txt");
 
     cout << tags::green_bold
-         << "\nCompiled " << objs.size() << " objects.\n"
+         << "Compiled " << objs.size() << " objects.\n"
          << tags::reset;
 
     return objs;
@@ -86,20 +124,31 @@ vector<string> makeObjs()
 void link(const vector<string> objs)
 {
     cout << tags::green_bold
-         << "Calling linker...\n\n"
+         << "Linking...\n"
          << tags::reset;
 
     string command = CC + ' ' + ARGS + ' ';
 
+    vector<string> mainObjs;
+
     for (string o : objs)
-        command += o + ' ';
+    {
+        if (regex_search(o, regex("/__MAIN__")))
+            mainObjs.push_back(o);
+        else
+            command += o + ' ';
+    }
 
-    command += "-o " + NAME;
+    for (string mainO : mainObjs)
+    {
+        string name = regex_replace(mainO, regex(".*/__MAIN__"), "");
+        name = regex_replace(name, regex("\\.o"), ".out");
 
-    smartSystem(command);
+        smartSystem(command + mainO + " -o bin/" + name);
+    }
 
     cout << tags::green_bold
-         << "\nSuccessfully linked.\n"
+         << "Successfully linked.\n"
          << tags::reset;
 
     return;
@@ -111,7 +160,6 @@ int main(const int argc, const char *argv[])
 {
     try
     {
-
         string tag;
         for (int i = 1; i < argc; i++)
         {
@@ -121,16 +169,30 @@ int main(const int argc, const char *argv[])
                 i++;
                 CC = argv[i];
             }
-            else if (tag == "-o" || tag == "--output")
-            {
-                i++;
-                NAME = argv[i];
-            }
             else if (tag == "-org")
             {
-                smartSystem("mkdir headers && mv *.h headers");
-                smartSystem("mkdir source && mv *.cpp source");
-                smartSystem("mkdir bin && mv *.out bin");
+                smartSystem("mkdir -p headers && mv *.h headers");
+                smartSystem("mkdir -p source && mv *.cpp source");
+                smartSystem("mkdir -p bin && mv *.out bin");
+                smartSystem("mkdir -p .build && mv *.o .build");
+            }
+            else if (tag == "-c" || tag == "--clean")
+            {
+                smartSystem("rm -f .build/*.o");
+            }
+            else if (tag == "-p" || tag == "--purge")
+            {
+                smartSystem("rm -f .build/*.o bin/*.out");
+            }
+            else if (tag == "-jc" || tag == "--justclean")
+            {
+                smartSystem("rm -f .build/*.o");
+                return 0;
+            }
+            else if (tag == "-jp" || tag == "--justpurge")
+            {
+                smartSystem("rm -f .build/*.o bin/*.out");
+                return 0;
             }
             else if (tag == "-h" || tag == "--help")
             {
@@ -138,11 +200,20 @@ int main(const int argc, const char *argv[])
                      << "Short version | Long version | Description\n"
                      << "------------------------------------------\n"
                      << "-u            | --use <CC>   | changes compilers\n"
-                     << "-o            | --output <P> | changes the output path\n"
                      << "              | -org         | organizes files\n"
                      << "-h            | --help       | shows this page\n"
                      << "              | -<argname>   | adds an argument to the compiler call\n"
-                     << "(Jorb Dehmel, 2022, jdehmel@outlook.com)\n"
+                     << "-c            | --clean      | removes .o files\n"
+                     << "-p            | --purge      | removes .o and .out files\n"
+                     << "-jc           | --justremove | cleans without compiling or linking\n"
+                     << "-jp           | --justpurge  | purges without compiling or linking\n"
+                     << "\nAutotagging:\n"
+                     << "   Insert \"// SC_ARGS <tags go here>\" as the beginning of\n"
+                     << "   a .cpp file to activate autotagging.\n"
+                     << "\nAutolinking:\n"
+                     << "   SmartCompile will automatically detect .cpp files with\n"
+                     << "   main functions and link them appropriately.\n"
+                     << "\n(Jorb Dehmel, 2022, jdehmel@outlook.com)\n"
                      << tags::reset;
 
                 return 0;
@@ -153,17 +224,30 @@ int main(const int argc, const char *argv[])
             }
         }
 
+        smartSystem("mkdir -p bin .build");
+
         link(makeObjs());
 
         cout << tags::green_bold
-             << "Completed.\n"
+             << "Completed. (Output .out file(s) can be found in ./bin/ )\n"
              << tags::reset;
+    }
+    catch (runtime_error &e)
+    {
+        cout << tags::red_bold
+             << "\nFatal runtime error encountered; Compilation canceled.\n"
+             << tags::reset
+             << e.what()
+             << '\n';
+
+        return 1;
     }
     catch (...)
     {
         cout << tags::red_bold
-             << "\nFatal error encountered; Canceling compilation...\n"
+             << "\nFatal error encountered; Compilation canceled.\n"
              << tags::reset;
+
         return 1;
     }
 
